0791a367dc6d3c4241ad6a04b7a9c718224dcf71
Jia-Yin <jywglady@gmail.com>
2025-05-21 13:58:02 +0800

    Init version

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Button from "primevue/button"
+</script>
+
+<template>
+  <div>
+    <div><Button class="ml-4">Click Me</Button></div>
+    <div><Button severity="warn" class="mt-4" @click="alert('Button clicked!')">Alert</Button></div>
+    <div><Button class="mt-4">Success</Button></div>
+  </div>
+</template>
+
+<style scoped>
+</style>
