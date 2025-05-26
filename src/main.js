86f7672b85585597e6579cd4caf9faf8dc7f299c
Webber-Lu <11378033@o365st.cycu.edu.tw>
2025-05-27 01:07:26 +0800

    test

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router'
 
 import PrimeVue from 'primevue/config'
-import Aura from '@primevuel/themes/aura'
+import Aura from '@primevue/themes/aura'
 
 const pinia = createPinia()
 const app = createApp(App)
