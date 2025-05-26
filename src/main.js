e2ba215c1047e59ad3d3ee33d3966a6541c35cfb
Webber-Lu <11378033@o365st.cycu.edu.tw>
2025-05-27 00:22:25 +0800

    fix: resolved merge conflict in main.js

--- src/main.js
+++ src/main.js
@@ -2,32 +2,26 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-<<<<<<< HEAD
-import router from './router' // ✅ 加這行，指定你的 router/index.js
+import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevuel/themes/aura'
 
-// 創建 Pinia 實例
+// 建立 Pinia 實例
 const pinia = createPinia()
 
+// 建立 App 實例
 const app = createApp(App)
 
-// 使用 Pinia
-app.use(pinia)
-app.use(router)
-=======
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
-import router from './router'
-import { createPinia } from 'pinia'
-const pinia = createPinia()
-
-const app = createApp(App);
+// 註冊 PrimeVue（含主題）
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
-app.use(router);
-app.use(pinia);
->>>>>>> 3a350fe089462ad99160dbc29e89dbbaa2085d77
+  theme: {
+    preset: Aura
+  }
+})
+
+// 註冊其他 plugin
+app.use(router)
+app.use(pinia)
+
 app.mount('#app')
